7e5687f2d046d7d0e25512515615f81acbfb20ca
Santi2906 <kschavarrod@correo.udistrital.edu.co>
2022-09-22 12:06:15 -0500

    mejorando la pagina

--- src/main/java/com/santiago/clase_sep_7_22/control.java
+++ src/main/java/com/santiago/clase_sep_7_22/control.java
@@ -29,7 +29,7 @@ public class control extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
+        try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
@@ -58,17 +58,46 @@ public class control extends HttpServlet {
         //processRequest(request, response);
         String n = request.getParameter("txt_nombres");
         String a = request.getParameter("txt_apellidos");
+        String t = request.getParameter("telefono");
+        String c = request.getParameter("txt_correo");
         response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
+        try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>respuesta 1</title>");
+            out.println("<title>respuesta 1</title>"
+                    + "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css\" \n"
+                    + "              rel=\"stylesheet\" integrity=\"sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3\" \n"
+                    + "              crossorigin=\"anonymous\">");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Esta es una respuesta desde el servlet" + request.getContextPath() + "</h1>");
-            out.println("<h1>El domicilio llego: " + n + " " + a + "</h1>");
+            out.println("<div class=\"container-fluid\">"
+                    + "<h1 class=\"text-center\">Bienvenido " + n + " " + a + " a mi página</h1>"
+                    + "<h3 class=\"text-center\">Sus datos son:</h3>"
+                    + "<div class=\"table-responsive\">"
+                    + "<table class=\"table table-hover table-ligth table-striped table-bordered\">"
+                    + "<thead>"
+                    + "<tr>"
+                    + "<th scope=\"col\">#</th>"
+                    + "<th scope=\"col\">Nombre</th>"
+                    + "<th scope=\"col\">Apellido</th>"
+                    + "<th scope=\"col\">Telefono</th>"
+                    + "<th scope=\"col\">Correo</th>"
+                    + "</tr>"
+                    + "</thead>"
+                    + "<tbody>"
+                    + "<tr>"
+                    + "<td>1</td>"
+                    + "<td>" + n + "</td>"
+                    + "<td>" + a + "</td>"
+                    + "<td>" + t + "</td>"
+                    + "<td>" + c + "</td>"
+                    + "</tr>"
+                    + "</tbody>"
+                    + "</table>"
+                    + "</div>"
+                    + "</div>");
             out.println("</body>");
             out.println("</html>");
         }
@@ -86,6 +115,22 @@ public class control extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
+        String n = request.getParameter("txt_nombres");
+        String a = request.getParameter("txt_apellidos");
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>respuesta 1</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Esta es una respuesta desde el servlet" + request.getContextPath() + "</h1>");
+            out.println("<h1>El domicilio llego: " + n + " " + a + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
 
     /**
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,25 +1,50 @@
-<%-- 
-    Document   : index
-    Created on : 7/09/2022, 1:14:30 p. m.
-    Author     : administradorPC
---%>
-
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Pagina 1</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
+              rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
+              crossorigin="anonymous">
+        <title>Santiago Chavarro</title>
     </head>
     <body>
-        <h1>Hola mundillo</h1>
-        <a href="control">Verificar servlet  objecto response</a>
-        <form action="control" method="GET">
-            <label>Nombres: </label>
-            <input type="text" name="txt_nombres" required="true">
-            <label>Apellidos: </label>           
-            <input type="text" name="txt_apellidos" required="true">
-            <button type="submit" name="btnRegistrar" value="Registrar">Registrar   </button>
-        </form>
+        <div class="container-fluid">
+            <h1 class="text-center">Bienvenido a mi pagina web con Java, GIT y Heroku</h1>
+            <div class="abs-center">
+                <form action="control" method="GET" class="border p-3 form">
+                    <div class="row g-3 align-items-center">
+                        <div class="mb-2 col-5 form-floating">
+                            <input type="text" name="txt_nombres" required="true" class="form-control" id="nombre" placeholder="Su nombre">
+                            <label for="nombre">Nombres: </label>
+                        </div>
+                        <div class="mb-2 col-5 form-floating">          
+                            <input type="text" name="txt_apellidos" required="true" class="form-control" id="apellido" placeholder="apellido">
+                            <label for="apellido">Apellidos: </label> 
+                        </div>
+                        <div class="mb-3 col-5 form-floating">
+                            <input type="number" name="telefono" required="true" class="form-control" autocomplete="off" id="tele" placeholder="tele">
+                            <label for="tele">Telefono: </label>
+                        </div>
+                        <div class="mb-3 col-5 form-floating">
+                            <input type="text" name="txt_correo" required="true" class="form-control" autocomplete="off" id="cor" placeholder="correo">
+                            <label for="cor">Correo: </label>
+                        </div>
+                    </div>
+                    <button type="submit" name="btnRegistrar"  
+                            class="btn btn-outline-primary" >Registrar</button>
+                    <button type="reset" class="btn btn-outline-danger">Limpiar</button>
+                </form>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
+                integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" 
+        crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" 
+                integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" 
+        crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" 
+                integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" 
+        crossorigin="anonymous"></script>
     </body>
 </html>
