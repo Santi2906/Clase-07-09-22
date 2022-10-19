f69f105de7e5166b629048529f234db8c6730c58
Santi2906 <kschavarrod@correo.udistrital.edu.co>
2022-10-19 16:10:37 -0500

    Agregando Siglenton a la conexion

--- src/main/java/com/santiago/Conexion/Conexion_Sigleton.java
+++ src/main/java/com/santiago/Conexion/Conexion_Sigleton.java
@@ -5,10 +5,41 @@
  */
 package com.santiago.Conexion;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Santiago
  */
 public class Conexion_Sigleton {
-    
+    private static Conexion_Sigleton instance;
+    private Connection connection;
+    private String url = "jdbc:postgresql://localhost:5432/jdbc";
+    private String username = "postgres";
+    private String password = "123456";
+
+    private Conexion_Sigleton() throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver");
+            this.connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Fallo la conexión a la base de datos : " + ex.getMessage());
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public static Conexion_Sigleton getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new Conexion_Sigleton();
+        } else if (instance.getConnection().isClosed()) {
+            instance = new Conexion_Sigleton();
+        }
+
+        return instance;
+    }
 }
